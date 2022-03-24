77de72c180fd8686e283a7fba8f611440ffae5b6
hugeblank <hugeblank@gmail.com>
2022-03-24 04:30:24 -0700

    Update examples
    I forgor, oops

--- examples/hangman/main.lua
+++ examples/hangman/main.lua
@@ -1,12 +1,12 @@
--- Hangman for Allium
--- by hugeblank - March 22, 2022
+-- Hangman 
+-- By hugeblank - March 22, 2022
+-- A game of hangman, played in the chat. Use !hangman to start a game, add a letter or word after to start guessing.
 -- Derived from the original !hangman command in alpha, for allium-cc
 -- Source: https://github.com/hugeblank/Alpha/blob/master/alpha.lua#L354
 
 local words = require "words"
 
 local function text(str)
-    -- Parse & convert tagged text to formatted text
     return texts.toJson(texts.parseSafe(str))
 end
 
@@ -14,8 +14,6 @@ local word
 local guessed
 local guesses = 0
 local function parseGuess()
-    --[[ Returns a string where all guessed letters are exposed,
-     and all unknown letters are obscured with an underscore ]]
     local out = ""
     for i = 1, #word do
         if guessed[i] then
@@ -31,59 +29,60 @@ local function parseGuess()
 end
 
 allium.onEvent("chat_message", function(e, player, message)
-    local strs = java.split(message, " ") -- Split the string into a table using java magic
-    if table.remove(strs, 1) == "!hangman" then -- Check if this is for us
-        if #strs == 0 and word ~= nil then -- If a game is ongoing, and the user tries to start a game
+    local strs = java.split(message, " ")
+    if table.remove(strs, 1) == "!hangman" then
+        if #strs == 0 and word ~= nil then
             commands.tellraw(player:getName():asString(), text("<red>No guess! Add a letter or word to guess</red>"))
             return
-        elseif #strs == 0 then -- Initialize the game!
+        elseif #strs == 0 then
             ingame = true
             word = java.split(words[math.random(1, #words)], "")
             guessed = {}
-            for i = 1, #word do -- Create a table that keeps track of the letters correctly guessed by players
+            for i = 1, #word do
                 guessed[i] = false
             end
-            guesses = 15 -- A lenient amount guesses
+            guesses = 15
             commands.tellraw("@a", text("Guess the word!"))
             commands.tellraw("@a", text("<bold>"..parseGuess().."</bold>"))
             commands.tellraw("@a", text("You have 10 guesses. Good luck!"))
             return
         end
-        if word ~= nil then -- Handle guesses if a game is being played
+        if word ~= nil then
             commands.tellraw("@a", text(player:getName():asString().." guessed <bold>"..strs[1].."</bold>"))
-            if #strs[1] == 1 then -- If the player is only guessing a letter
+            if #strs[1] == 1 then
                 local correct = false
-                for i = 1, #word do -- Check each letter
-                    if word[i] == strs[1] then -- If the guessed letter exists in the word
-                        correct = true -- Flag to tell chat the player guessed correctly
-                        guessed[i] = true -- Flag that this letter has been guessed in the word, for parsing the guess
+                for i = 1, #word do
+                    if word[i] == strs[1] then
+                        correct = true
+                        guessed[i] = true
                     end
                 end
                 if correct then
                     commands.tellraw("@a", text("<green>"..player:getName():asString().." guessed a letter correctly!</green>"))
+                    guesses = guesses-1
                 else
                     commands.tellraw("@a", text("<red>"..player:getName():asString().." guessed a letter incorrectly!</red>"))
-                    guesses = guesses-1 -- Subtract a guess only if the guess is incorrect
                 end
-            else -- The player is guessing more than one letter
-                if strs[1] == table.concat(word, "") then -- if it's correct
+            else
+                if strs[1] == table.concat(word, "") then
                     commands.tellraw("@a", text("<green>"..player:getName():asString().." guessed the word! It was: <bold>"..table.concat(word, "").."</bold></green>"))
                     word = nil
                     guesses = 0
                     return
-                else -- If it's incorrect
-                    commands.tellraw("@a", text("<red>"..player:getName():asString().." guessed the word incorrectly! </red>"))
+                else
+                    commands.tellraw("@a", text("<red>"..player:getName():asString().." guessed incorrectly! </red>"))
                     guesses = guesses-1
                 end
             end
-            if guesses > 0 then -- The game goes on
+            if guesses > 0 then
                 commands.tellraw("@a", text("<bold>"..parseGuess().."</bold>"))
                 local s = " guesses"
-                if guesses == 1 then s = " guess" end -- handle the english language
+                if guesses == 1 then s = " guess" end
                 commands.tellraw("@a", text(tostring(guesses)..s.." left"))
             else
+                commands.tellraw("@a", text("<green>"..player:getName():asString().." guessed the word! </green>"))
                 commands.tellraw("@a", text("<red><bold>Game over!</bold> The word was: <bold>"..table.concat(word, "").."</bold></red>"))
-                word = nil -- Out of guesses, end the game
+                word = nil
             end
         end
     end
--- examples/wetworks/wetworks.lua
+++ examples/wetworks/wetworks.lua
@@ -1,7 +1,10 @@
--- Play around with the returned function, run !reload in game when you want to test it
+-- Wetworks
+-- By hugeblank - March 22, 2022
+-- Applies the 1.19 mud water bottle mechanic to concrete powder blocks
+-- This file is marked as a dynamic entrypoint in the manifest.json.
+-- Try modifying, and running /reload!
 
 -- Import java classes
-local Blocks = java.import("Blocks")
 local Items = java.import("Items")
 local Registry = java.import("Registry")
 local Identifier = java.import("Identifier")
@@ -10,17 +13,18 @@ local SoundCategory = java.import("SoundCategory")
 local EquipmentSlot = java.import("EquipmentSlot")
 
 -- Return a function that we can modify while the game is playing
-return function(e, state, world, pos, player, hand, hitResult)
+local listener = allium.onEvent("block_interact", function(e, state, world, pos, player, hand, hitResult)
     local concrete = Registry.BLOCK:getId(state:getBlock()):getPath() -- Get the name of the block interacted with
     local mainHand = player:getEquippedStack(EquipmentSlot.MAINHAND) -- Get the main hand itemstack of the player
     -- Check if the block name has 'concrete_powder' in it, then check if the main hand is holding a water bottle
-    if concrete:find("concrete_powder") and mainHand:isItemEqual(Items.POTION:getDefaultStack()) then 
+    if concrete:find("concrete_powder") and mainHand:isItemEqual(Items.POTION:getDefaultStack()) then
         -- Replace the powder block with the concrete variant
-        local state = Registry.BLOCK:get(Identifier("minecraft:"..concrete:gsub("_powder", ""))):getDefaultState()
-        world:setBlockState(pos, state)
+        world:setBlockState(pos, Registry.BLOCK:get(Identifier("minecraft:"..concrete:gsub("_powder", ""))):getDefaultState())
         -- Play the water bottle emptying sound effect
         world:playSound(nil, pos:getX(), pos:getY(), pos:getZ(), SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1, 1)
-        mainHand:setCount(0) -- Remove the water bottle
-        player:equipStack(EquipmentSlot.MAINHAND, Items.GLASS_BOTTLE:getDefaultStack()) -- Replace it with an empty glass bottle
+        if (not player:isCreative()) then -- If the player isn't in creative
+            mainHand:setCount(0) -- Remove the water bottle
+            player:equipStack(EquipmentSlot.MAINHAND, Items.GLASS_BOTTLE:getDefaultStack()) -- Replace it with an empty glass bottle
+        end
     end
-end
+end)
