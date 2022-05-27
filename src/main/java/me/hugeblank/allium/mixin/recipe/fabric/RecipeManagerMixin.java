73b1520ca3206f8941dc64e541ac0ea9fda0aa5f
Basique Evangelist <basiqueevangelist@yandex.ru>
2022-05-27 13:44:06 +0300

    make recipe modification compatible with QSL

---
+++ src/main/java/me/hugeblank/allium/mixin/recipe/fabric/RecipeManagerMixin.java
@@ -0,0 +1,18 @@
+package me.hugeblank.allium.mixin.recipe.fabric;
+
+import net.minecraft.recipe.RecipeManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Mixin(RecipeManager.class)
+public class RecipeManagerMixin {
+    @Inject(method = "method_20703", at = @At("RETURN"), cancellable = true)
+    private static <K, V> void makeSubMapMutable(Map.Entry<K, V> entry, CallbackInfoReturnable<Map<K, V>> cir) {
+        cir.setReturnValue(new HashMap<>(cir.getReturnValue()));
+    }
+}
---
+++ src/main/java/me/hugeblank/allium/util/CursedMapUtil.java
@@ -0,0 +1,18 @@
+package me.hugeblank.allium.util;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Collections;
+import java.util.Map;
+
+public final class CursedMapUtil {
+    private CursedMapUtil() {
+
+    }
+
+    @SuppressWarnings("RedundantUnmodifiable") private static final Class<?> UNMODIFIABLE_MAP = Collections.unmodifiableMap(Collections.emptyMap()).getClass();
+
+    public static boolean isUnmodifiableMap(Map<?, ?> map) {
+        return map instanceof ImmutableMap<?,?> || map.getClass() == UNMODIFIABLE_MAP;
+    }
+}
